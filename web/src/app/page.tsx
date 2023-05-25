e22f5faef77ecd039f36c5ff2d4315a415c14952
Ana Clara <anaclaranoronha.m@gmail.com>
2023-05-25 11:05:59 -0300

    Aula 2 - 34:21

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>aloo</h1>
-  )
+    <main className="grid min-h-screen grid-cols-2">
+      <div className=" relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes"/>
+      </div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underlined hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
